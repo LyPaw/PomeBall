c81cca6e1234eeb503414b21e98e0230cc18d289
Manuel Fuentes Cruz <githubmfcruzzz@gmail.com>
2026-04-25 11:51:10 +0200

    Delete src directory

--- src/Inventario.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Inventario {
-     private ArrayList<Item> items = new ArrayList<>();
-     
-     public void agregarItem(Item item) { items.add(item); }
-     public ArrayList<Item> getItems() { return items; }
-}
--- src/TipoItem.java
+++
@@ -1,6 +0,0 @@
-public enum TipoItem {
-    Equipable,
-    Curacion,
-    Evolutivo,
-    PokeBall;
-}
